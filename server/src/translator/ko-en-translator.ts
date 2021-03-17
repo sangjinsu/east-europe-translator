b166f9c0c300b64792dd718e2cad7df7eed52788
jinsuSang <twtz0309@naver.com>
2021-03-17 18:10:09 +0900

    enhancement: create translator classes

--- server/src/translator/ko-en-translator.ts
+++
@@ -1,8 +0,0 @@
-import { TranslateBehavior } from 'src/translateBehavior/translate-behavior.interface'
-import { Translator } from './translator'
-
-export class KoEnTranslator extends Translator {
-  constructor(translatorBehavior: TranslateBehavior) {
-    super(translatorBehavior, 'ko', 'en')
-  }
-}
