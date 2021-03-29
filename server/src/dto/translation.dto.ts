d8321e53be10107b2de9f52daeeb29a76f25320b
jinsuSang <twtz0309@naver.com>
2021-03-29 21:10:37 +0900

    enhancement: change filter string length to 1000

--- server/src/dto/translation.dto.ts
+++ server/src/dto/translation.dto.ts
@@ -3,6 +3,6 @@ import { IsString, IsNotEmpty, Length } from 'class-validator'
 export class TranslationDto {
   @IsString()
   @IsNotEmpty()
-  @Length(1, 3000)
+  @Length(1, 1000)
   text: string
 }
